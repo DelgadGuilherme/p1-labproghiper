ff53ea0e50602378cfd717bbb482c12ee83961ae
Guilherme <guilhermedms@hotmail.com>
2020-05-29 15:06:34 -0300

    Fix: Nova estanciação
    Foi refeita a estanciação dos videos para dentro do __init__.py para que os comentarios nao sejam perdidos de uma requisição para outra

--- application/model/dao/categoria_dao.py
+++ application/model/dao/categoria_dao.py
@@ -3,16 +3,16 @@ from application.model.entity.video import Video
 
 class CategoriaDAO:
     def __init__(self):
-        video1 = Video(1,"De boas","Um gato de boas se espreguiçando",'img/img_video/gato1.png','video/gato1.mp4')
-        video2 = Video(2,"Hora de nanar","Uma gata indo dormir com seus filhotinhos",'img/img_video/gato2_1.png','video/gato2.mp4')
-        video3 = Video(3,"Iti, o fofo","Uum cachorro dentro da caixa de boca aberta",'img/img_video/cachorro1.png','video/cachorro2.mp4')
-        video4 = Video(4,"Hora da diversão","Dois cachorros brincando no parque",'img/img_video/cachorro2.png','video/cachorro1.mp4')
+        self._video_list = [video1,video2,video4,video3]
         self._categoria_list = []
         self._categoria_list.append(Categoria(1,"Gatinhos","Nessa categoria voce pode ver uns gatos muitos fofos",'img/img_categoria/gato.jpg',[video1,video2]))
         self._categoria_list.append(Categoria(2,"Cachorrinhos","Nessa categoria voce pode ver uns cachorros muitos fofos",'img/img_categoria/cachorro.png',[video3,video4]))
 
     def listar(self):
         return self._categoria_list
+
+    def listar_mais_curtidas(self):
+        return self._video_list
 
     def buscar_por_id(self, id):
         for i in range(0, len(self._categoria_list)):
